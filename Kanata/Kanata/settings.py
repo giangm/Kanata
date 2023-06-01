2bad53282e26e28a1578497a5f0992dcca8944f9
Minh Giang <giangm@sheridancollege.ca>
2023-06-01 02:36:44 -0400

    updates to the setup

--- Kanata/Kanata/settings.py
+++ Kanata/Kanata/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'rest_framework',
+    'corsheaders',
+    'app.apps.AppConfig'
 ]
 
 MIDDLEWARE = [
@@ -47,14 +52,20 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+REST_FRAMEWORK = {"DEFAULT_PERMISSION_CLASSES": [
+    "rest_framework.permissions.AllowAny"]}
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'Kanata.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'frontend')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'frontend', "build", "static"),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Kanata/api/views.py
+++ Kanata/api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+import docker
+
+client = docker.from_env()
 
 # Create your views here.
+class ChallengeListView(APIView):
+    def get(self, request, format=None):
+        containers = {}
+        counter = 0
+        for container in client.containers.list():
+            print(container)
+            nickname = container.name
+            labels = container.labels
+            cstatus = container.status
+            shortId = container.short_id
+            attrs = container.attrs
+
+            containers[counter] = [nickname, cstatus, labels["name"], labels["desc"], shortId, attrs]
+            counter += 1
+
+        return Response({"data": containers}, status=status.HTTP_200_OK)
